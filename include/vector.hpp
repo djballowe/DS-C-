63ec2d73e995106e571426407a9f3351d9f0808d
djballowe <djballowe@gmail.com>
2025-10-29 19:37:42 -0700

    add destory + capacity

--- include/vector.hpp
+++ include/vector.hpp
@@ -1,25 +1,41 @@
 #pragma once
+#include <memory>
 
-template <typename T>
-class my_vector {
-    public: 
-        my_vector(T val);
+template <typename T> class my_vector {
+  public:
+    my_vector(T val);
 
-        int size() const;
+    ~my_vector();
 
-    private:
-        int* init;
-        
+    size_t size() const;
 
+    size_t capacity() const;
+
+    T &index(int idx) const;
+
+  private:
+    std::allocator<int> block;
+    int *head;
+    size_t m_length;
+    size_t length;
 };
 
-template <typename T>
-my_vector<T>::my_vector(T val) : init([](T v) {
-    return new int[v];
-}(val)) {
+template <typename T> my_vector<T>::my_vector(T val) : block(), head(block.allocate(val * 2)), length(val), m_length() {
+    this->m_length = val * 2;
+    for (size_t i = 0; i < this->m_length; i++) {
+        block.construct(head + i, 5);
+    }
 }
 
-template <typename T>
-int my_vector<T>::size() const {
-    return this->init[0];
+template <typename T> my_vector<T>::~my_vector() {
+    for (size_t i = 0; i < this->length; i++) {
+        this->block.destroy(this->head + i);
+    }
+    block.deallocate(this->head, this->m_length);
 }
+
+template <typename T> size_t my_vector<T>::size() const { return this->length; }
+
+template <typename T> size_t my_vector<T>::capacity() const { return this->m_length; }
+
+template <typename T> T &my_vector<T>::index(int idx) const { return *(this->head + idx); }
--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,15 @@
 #include "../include/vector.hpp"
 #include <iostream>
 
-int main () {
-    my_vector<int> test_vector(5);
+int main() {
+    my_vector<int> test_vector(2);
 
-    std::cout << test_vector.size() << std::endl;
+    std::cout << "my vector size: " << test_vector.size() << std::endl;
+    std::cout << "my vector total capacity: " << test_vector.capacity() << std::endl;
+
+    for (size_t i = 0; i < test_vector.size(); i++) {
+        std::cout << "index " << i << " = " << test_vector.index(i) << std::endl;
+    }
 
     return 0;
 }
