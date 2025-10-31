ec4d0e197bb5ade0846c5438efcd06d91fc07c97
djballowe <djballowe@gmail.com>
2025-10-30 21:04:40 -0700

    add front back and init list

--- include/vector.hpp
+++ include/vector.hpp
@@ -1,29 +1,38 @@
 #pragma once
+#include <initializer_list>
+#include <iostream>
 #include <memory>
 
 template <typename T> class my_vector {
   public:
-    my_vector(T val);
+    my_vector();
+    my_vector(std::initializer_list<T> vals);
 
     ~my_vector();
 
     size_t size() const;
-
     size_t capacity() const;
 
     T &index(int idx) const;
 
+    T &front() const;
+    T &back() const;
+
   private:
     std::allocator<int> block;
     int *head;
-    size_t m_length;
+    size_t m_capacity;
     size_t length;
 };
 
-template <typename T> my_vector<T>::my_vector(T val) : block(), head(block.allocate(val * 2)), length(val), m_length() {
-    this->m_length = val * 2;
-    for (size_t i = 0; i < this->m_length; i++) {
-        block.construct(head + i, 5);
+template <typename T> my_vector<T>::my_vector() : block(), head(nullptr), length(0), m_capacity(0) {}
+
+template <typename T>
+my_vector<T>::my_vector(std::initializer_list<T> vals)
+    : block(), head(block.allocate(vals.size() * 2)), length(vals.size()), m_capacity(vals.size() * 2) {
+    size_t idx = 0;
+    for (const T &val : vals) {
+        block.construct(head + (idx++), val);
     }
 }
 
@@ -31,11 +40,15 @@ template <typename T> my_vector<T>::~my_vector() {
     for (size_t i = 0; i < this->length; i++) {
         this->block.destroy(this->head + i);
     }
-    block.deallocate(this->head, this->m_length);
+    block.deallocate(this->head, this->m_capacity);
 }
 
 template <typename T> size_t my_vector<T>::size() const { return this->length; }
 
-template <typename T> size_t my_vector<T>::capacity() const { return this->m_length; }
+template <typename T> size_t my_vector<T>::capacity() const { return this->m_capacity; }
 
 template <typename T> T &my_vector<T>::index(int idx) const { return *(this->head + idx); }
+
+template <typename T> T &my_vector<T>::back() const { return *(this->head + this->length - 1); }
+
+template <typename T> T &my_vector<T>::front() const { return *(this->head); }
--- src/main.cpp
+++ src/main.cpp
@@ -2,13 +2,16 @@
 #include <iostream>
 
 int main() {
-    my_vector<int> test_vector(2);
+    my_vector<int> custom_vector = {1, 2, 3, 4, 5};
 
-    std::cout << "my vector size: " << test_vector.size() << std::endl;
-    std::cout << "my vector total capacity: " << test_vector.capacity() << std::endl;
+    std::cout << "my vector size: " << custom_vector.size() << std::endl;
+    std::cout << "my vector total capacity: " << custom_vector.capacity() << std::endl;
 
-    for (size_t i = 0; i < test_vector.size(); i++) {
-        std::cout << "index " << i << " = " << test_vector.index(i) << std::endl;
+    std::cout << "my vector front reference: " << custom_vector.front() << std::endl;
+    std::cout << "my vector back reference: " << custom_vector.back() << std::endl;
+
+    for (size_t i = 0; i < custom_vector.size(); i++) {
+        std::cout << "index " << i << " = " << custom_vector.index(i) << std::endl;
     }
 
     return 0;
